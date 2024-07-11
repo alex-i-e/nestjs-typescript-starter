40ca1ed7304d9b01a4053b85575d547bd0f81ab9
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-07-11 15:41:45 +0300

    feat: added common config with dummy mongodb-config

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+import { ConfigProps } from 'src/interfaces/config.interfaces';
+
+export const config = (): ConfigProps => ({
+  port: parseInt(process.env.PORT, 10) || 8080,
+  api: {
+    apiUrl: process.env.API_URL,
+    httpTimeout: 1000,
+  },
+  mongodb: {
+    database: {
+      connectionString:
+        process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017',
+      databaseName: process.env.NODE_ENV || 'local',
+    },
+  },
+});
