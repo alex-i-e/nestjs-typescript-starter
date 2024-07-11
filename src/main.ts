ad2296e949ce988690814ed2b753c5346ba297ae
Aleksandr E <aleksandr.i.eremin@yandex.ru>
2024-07-11 17:06:37 +0300

    feat: added @nestjs/config dependency and applied config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+  await app.listen(port);
+  Logger.log(`~ Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
